53b3770b1b8bb1a03f6d7e9f89235d9a5ea94ba8
Fabianus Harry Setiawan <52847215+fabianusharry@users.noreply.github.com>
2021-10-02 20:39:48 +0700

    insert model for schema

--- backend/dictionary/types.go
+++ backend/dictionary/types.go
@@ -1,11 +1,39 @@
 package dictionary
 
+import (
+	"time"
+)
+
 type Product struct {
 	ID           int64   `json:"id"`
 	Name         string  `json:"name"`
 	ShopName     string  `json:"shop_name"`
 	ProductPrice float64 `json:"product_price"`
 	ImageURL     string  `json:"image_url"`
+}
+
+type Banner struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ImgPath     string    `json:"img_path"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     time.Time `json:"end_date"`
+}
+
+type Tag struct {
+	ID  int    `json:"id"`
+	Tag string `json:"tag"`
+}
+
+type User struct {
+	ID            int    `json:"id"`
+	Age           string `json:"age"`
+	Region        string `json:"region"`
+	Gender        string `json:"gender"`
+	Tier          string `json:"tier"`
+	WalletBalance int    `json:"wallet_balance"`
+	ProductPref   string `json:"product_pref"`
 }
 
 type APIResponse struct {
